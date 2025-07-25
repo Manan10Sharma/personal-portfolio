1a2384789403753e147ebbc5e186eed892af3eb1
Manan10Sharma <manansharma1151@gmail.com>
2025-07-26 02:35:02 +0530

    Updated features

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -5,6 +5,7 @@ export const ProjectCard = ({ title, description, imgUrl }) => {
     <Col sm={6} md={4}>
       <div className="proj-imgbx">
         <img src={imgUrl} alt={title} />
+        {/* The .proj-imgbx::before pseudo-element still provides an overlay for stylistic purposes */}
         <div className="proj-txtx">
           <h4>{title}</h4>
           <span>{description}</span>
